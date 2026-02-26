c151837a33a3d1381942183b0333afed27d3923f
JanainaLopes <janainammesquita@hotmail.com>
2026-02-26 00:04:20 -0300

    criando o arquivo auth service

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,12 +1,21 @@
 const bcrypt = require("bcrypt")
+
 const jwt = require("jsonwebtoken")
-const prisma = require("../../database/prisma")
+
+const secret_key = 'Janaina';
+
+const prisma = require('../../database/prisma')
 
 exports.login = (username, password) => {
     const user = prisma.user.findUnique({
-        where: {
-            username: username
-        }
+    where: {
+        username: username
+    }
     })
 
+    const passwordIsvalid = bcrypt.compareSync
+
+    return jwt.sign({id: user.id, name: user.name}, secret_key,{expiresIn:"3h"})
+
 }
+
